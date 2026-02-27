549c47daf2c7d06877f256d4d96137021b4fde23
ministryofexpatriates81-arch <ministryofexpatriates81@gmail.com>
2026-02-27 23:48:20 +0600

    Add Firebase configuration file

---
+++ src/config/firebaseAdmin.js
@@ -0,0 +1,11 @@
+// Firebase Admin SDK configuration
+
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./path/to/serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+module.exports = admin;
